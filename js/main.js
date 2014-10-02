4495bf46ca43e0fbb3542499ee09e2b1d19b77a8
Rich Ramsland <rramsland@gmail.com>
2014-10-02 19:01:01 -0400

    updated code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-	$(".col1").click(function{
-		$(this).css("background-image")
-	})
-})
+	var col1Count=0;
+	$(".col").click(function(){
+		alert($(this).css("background-image"));
+	});
+	$(".col1").click(function(){
+		if (col1Count=0)
+		{
+			$(".col1").css("background-image",'url("http://fc01.deviantart.net/fs17/f/2007/129/7/4/Stock_032__by_enchanted_stock.jpg")');
+			col1Count++;
+		}
+		
+	});
+});
